97ecda3966945c7cafadd3ae5ed8f1acd01e87a5
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2019-02-21 21:40:39 -0700

    functional!

---
+++ src/components/HistoricData.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Data(props) {
+    return (
+        <div className="targetScore">
+            <h3>Wins: {props.wins}</h3>
+            <h3>Losses: {props.losses}</h3>
+        </div>
+    );
+}
+
+export default Data
